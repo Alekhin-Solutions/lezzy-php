2aef66c1c68837ced3a433a877d2e34d26ee0ff6
Japa Alekhin Llemos <jap@alekhin.io>
2021-02-18 23:17:02 +0800

    initial structure

---
+++ src/AlekhinSolutions/Lezzy/Lezzy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AlekhinSolutions\Lezzy;
+
+class Lezzy
+{
+
+    public static function score($needle, $straw)
+    {
+    }
+
+    public static function search($needle, $haystack)
+    {
+    }
+}
